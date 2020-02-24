98aa20002fcd44c623ea2975217fd6f153f12a2d
jacoblindberg18 <jacob.lindberg18@gmail.com>
2020-02-24 17:21:09 +0100

    finished the filtering of objects when specific button is clicked, finished the rendering for said specific object, and also finished rendering for all objects

--- google-maps.js
+++ google-maps.js
@@ -1,4 +1,5 @@
 const h1 = document.getElementById("h1");
+const apartmentArea = document.getElementById("apartment-area");
 const buttons = document.querySelectorAll(".button");
 
 getApartments = async () => {
@@ -8,8 +9,8 @@ getApartments = async () => {
         const { data: apartments } = await result;
 
         console.log("this is apartments", apartments);
-        h1.innerHTML += `${apartments.apartments[0].city}`;
-        addEventListeners();
+        addEventListeners(apartments.apartments);
+        renderAllApartments(apartments.apartments);
     } catch (err) {
         console.log("ERROR", err);
     }
@@ -23,13 +24,56 @@ function removeActiveClass() {
     });
 }
 
-function addEventListeners() {
+function addEventListeners(apartments) {
     buttons.forEach(button => {
-        console.log("hej", button);
         button.addEventListener("click", () => {
             removeActiveClass();
             button.classList.add("active");
-            console.log(button.innerText);
+            if (button.innerText !== "All") {
+                let filtered = filterApartments(apartments, button.innerText);
+                renderFilteredApartments(filtered);
+            } else {
+                renderAllApartments(apartments);
+            }
         });
+    });
+}
+
+function filterApartments(allApartments, buttonText) {
+    let filtered = allApartments.filter(apartment => {
+        return apartment.city === buttonText;
+    });
+    return filtered;
+}
+
+function renderFilteredApartments(apartments) {
+    apartmentArea.innerHTML = "";
+    apartments.forEach(apartment => {
+        apartmentArea.innerHTML += `<a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+            <div>
+                <p class="mb-1">${apartment.description}</p>
+                <p class="mb-1">${apartment.address}</p>
+            </div>
+            <small>${apartment.price}</small>
+        </div>
+        <small>Bedrooms: ${apartment.bedrooms} / Neighborhood: ${apartment.neighborhood}</small>
+    </a>`;
+    });
+}
+
+function renderAllApartments(apartments) {
+    apartmentArea.innerHTML = "";
+    apartments.forEach(apartment => {
+        apartmentArea.innerHTML += `<a href="#" class="list-group-item list-group-item-action">
+        <div class="d-flex w-100 justify-content-between">
+            <div>
+                <p class="mb-1">${apartment.description}</p>
+                <p class="mb-1">${apartment.address}</p>
+            </div>
+            <small>${apartment.price}</small>
+        </div>
+        <small>Bedrooms: ${apartment.bedrooms} / Neighborhood: ${apartment.neighborhood}</small>
+    </a>`;
     });
 }
